d9b3b0ee7faa972322d22b25edd22583a2337548
satviksinha <74920758+satviksinha@users.noreply.github.com>
2020-11-26 00:12:54 +0530

    created the following program

---
+++ task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int x,y,z;
+ cout<<"Enter x y and z";
+ cin>>x>>y>>z;
+ if(x-(y%x)>=z)
+{
+ cout<<"NO";
+} 
+else
+{
+ cout<<"YES";
+}
+   return 0;
+}
