c876b732cd7bbb69f3adbc251e0710db1951a3df
satviksinha <74920758+satviksinha@users.noreply.github.com>
2020-11-28 21:25:14 +0530

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main()
 {
-    int n,k,i,j,count=0,flag;
-    char s[50],dum[50];
+    int n,k,i,j,count=0,flag,l=0,c,p;
+    char s[50],dum[50],temp[100];
     cout<<"Enter the values of n and k"<<endl;
     cin>>n>>k;
     cout<<"Enter the string"<<endl;
     cin>>s;
-    cout<<"Enter the k characters";
+    cout<<"Enter the "<< k<<" characters";
     for(i=0;i<k;i++)
     {
         cin>>dum[i];
@@ -28,26 +28,40 @@ int main()
         }
     
     }
-    for(i=0;i<n;i++)
+    for(int len=1;len<=n;len++)
     {
-      for(int len=1;len<=n;len++)
-      { 
-       
-      
-          for( j=i;j<=(i+len);j++)
-          {
-            if(s[i+len]=='A')
-            break;
-           if(s[j]!='A' && j!=(n-1))
-           cout<<s[j];
-          }
-      
-          count++;
-         }
+      for(i=0;i<=(n-len);i++)
+      {
+        j= i+len-1;
+        for(int w=i;w<=j;w++)
+        {
+        temp[l]=s[w];
+        l++;
+        }
+        temp[l]=' ';
+        l++;
       }
-      
+    }
+    c=l;
+    i=0; j=0;
+    while(i!=c)
+    { p=0;
+     for(j=i;j!=' ';j++)
+     {
+       if(temp[j]=='A')
+         p=1;
+     }
+     if(p!=1)
+     {
+      for(k=i;k!=' ';k++)
+      cout<<temp[k];
+      i=k+1;
+     }
+     else
+     {
+     i=j+1;
+     }
+    }
     
-    cout<<count;
-
     return 0;
 }
